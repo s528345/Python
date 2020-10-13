b1c18c31b88e7f97981a6f9af2a9f0961239d092
Chase Staples <s528345@nwmissouri.edu>
2020-10-12 19:25:48 -0500

    Creating neural networks to be trained

--- AI Chatbot/AI Chatbot.py
+++ AI Chatbot/AI Chatbot.py
@@ -4,7 +4,7 @@ stemmer = lancasterStemmer()
 
 import numpy
 import tflearn
-import tensorflow
+import tensorflow as tf
 import random
 import json
 
@@ -21,14 +21,17 @@ for intents in data["intents"]:
 	for patterns in intents["patterns"]
 		wrds = nltk.word_tokenize(patterns)
 		words.extend(wrds)
-		doc_x.append(patterns)
+		doc_x.append(wrds)
 		docs_y.append(intents["tag"])
 
 	if intents["tag"] not in labels:
 		label.append(intents["tag"])
 
 # Words sorted and to lower case in json
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) 
+			for w in words 
+				if w != "?"
+		]
 words = sorted(List(set(words)))
 
 lablels = sorted(labels)
@@ -36,7 +39,7 @@ lablels = sorted(labels)
 training = []
 output = []
 
-out_empty = [0 for _ in range(len(classes))]
+out_empty = [0 for _ in range(len(labels))]
 
 for x, doc in enumerate(docs_x):
 	bag = []
@@ -55,5 +58,20 @@ for x, doc in enumerate(docs_x):
 	training.append(bag)
 	ouput.append(output_row)
 
-training = numoy.array(training)
+training = numpy.array(training)
 output = np.array(output)
+
+tf.reset_default_graph()
+
+# Neuron Layers / Neural Network
+net = tflearn.input_data(shape=[none, len(training[0])]) 					# Input data / training data
+net = tflearn.fully_connected(net, 8) 					 					# Hidden layer
+net = tflearn.fully_connected(net, 8)									    # Hidden layer
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")    # Output layer
+net = tflearn.regression(net)
+
+#Model to be trained
+model = tflearn.out(net) 
+
+model.fit(training, output, n_epoch = 1000, batch_size = 8, show_metric = True)
+model.save("model.tflearn")
