11692133ab401cec2079f9cc88be38747f8e1947
Chase Staples <s528345@nwmissouri.edu>
2020-10-28 20:31:13 -0500

    Changes made to main

--- Euler Problems/main.py
+++ Euler Problems/main.py
@@ -1,19 +1,29 @@
-import IFS
+import EulerProblems
+
+def prompt():
+    choice = input("Choose a Euler problem (1-10) to solve or 0 to quit: ")
+    return choice
+
+def choice(number):
+
+    if (number == 1):
+        print("Solution to Euler's Problem 1: ", EulerProblems.problem001(), "\n")
+
+    if(number == 2):
+        print("Solution to Euler's Problem 2: ", EulerProblems.problem002(), "\n")
 
 if __name__ == '__main__':
-    choice = IFS.prompt()
-    while choice != 'exit':
-        if choice == 'exit':
-            print("Goodbye!")
+    problem = int(prompt())
+    while(problem != 0):
+        if (problem > 3):
+            print("Oops! Try Again\n")
+            problem = int(prompt())
+        if(problem == 0):
             break
+        choice(problem)
+        problem = int(prompt())
+    print("Goodbye!")
 
-        if choice != 'exit' and int(choice) < 3:
-            number = int(choice)
-            IFS.choice(number)
-            choice = IFS.prompt()
-        if(str(choice) != 'exit' and int(choice) > 2):
-            print("\nOops! Try again\n")
-            choice = IFS.prompt()
 
 
 
